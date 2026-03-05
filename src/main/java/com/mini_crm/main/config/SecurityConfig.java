c78a32b34d73da0fa234de56e0df60897be9c3d6
duccanhole <duccanhole@gmail.com>
2026-03-05 23:42:00 +0700

    update config for sse api

--- src/main/java/com/mini_crm/main/config/SecurityConfig.java
+++ src/main/java/com/mini_crm/main/config/SecurityConfig.java
@@ -32,8 +32,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        // use for test
-                        // .requestMatchers("/api/sse/**").permitAll()
+                        .requestMatchers("/api/sse/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
